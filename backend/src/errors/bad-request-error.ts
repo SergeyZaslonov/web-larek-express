b5453ecc49625f1b8f0d142ad42e7d79028c1346
SergeyZaslonov <159817406+SergeyZaslonov@users.noreply.github.com>
2025-06-24 19:06:27 +0300

    errors

---
+++ backend/src/errors/bad-request-error.ts
@@ -0,0 +1,10 @@
+import { constants } from 'http2';
+
+export class BadRequestError extends Error {
+  public statusCode: number;
+
+  constructor(message: string) {
+    super(message);
+    this.statusCode = constants.HTTP_STATUS_BAD_REQUEST;
+  }
+}
---
+++ backend/src/errors/conflict-error.ts
@@ -0,0 +1,10 @@
+import { constants } from 'http2';
+
+export class ConflictError extends Error {
+  public statusCode: number;
+
+  constructor(message: string) {
+    super(message);
+    this.statusCode = constants.HTTP_STATUS_CONFLICT;
+  }
+}
