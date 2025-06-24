24951f07e5fb1dd06f3d95690e7fbd0ee405471e
SergeyZaslonov <159817406+SergeyZaslonov@users.noreply.github.com>
2025-06-24 19:39:22 +0300

    validatons.ts

--- backend/src/middlewares/validatons.ts
+++ backend/src/middlewares/validatons.ts
@@ -2,12 +2,11 @@ import { celebrate, Joi } from 'celebrate';
 
 export const validateProductBody = celebrate({
   body: Joi.object().keys({
-    title: Joi.string().required().max(30).min(2)
-      .messages({
-        'string.min': 'Минимальная длина поля title=2.',
-        'string.max': 'Максимальная длина поля title=30.',
-        'any.required': 'Поле title должно быть заполнено.',
-      }),
+    title: Joi.string().required().max(30).min(2).messages({
+      'string.min': 'Минимальная длина поля title=2.',
+      'string.max': 'Максимальная длина поля title=30.',
+      'any.required': 'Поле title должно быть заполнено.',
+    }),
     image: Joi.object()
       .required()
       .messages({ 'any.required': 'Поле image должно быть заполнено.' })
@@ -23,6 +22,7 @@ export const validateProductBody = celebrate({
       'any.required': 'Поле category должно быть заполнено.',
     }),
     price: Joi.number().allow(null),
+    description: Joi.string(),
   }),
 });
 
@@ -37,17 +37,18 @@ export const validateOrderBody = celebrate({
       'any.required': 'Поле email должно быть заполнено.',
     }),
     phone: Joi.string()
-      .regex(/\+[0-9]+\s\(\d\d\d\)\s\d\d\d\s\d\d\s\d\d/i).required().messages({
+      .regex(/\+[0-9]+\s\(\d\d\d\)\s\d\d\d\s\d\d\s\d\d/i)
+      .required()
+      .messages({
         'string.pattern.base':
-         'Поле телефона должно соответствовать шаблону +7 (999) 999-99-99.',
+          'Поле телефона должно соответствовать шаблону +7 (999) 999-99-99.',
         'any.required': 'Поле phone должно быть заполнено.',
       }),
-    address: Joi.string().required().min(4).max(100)
-      .messages({
-        'any.required': 'Поле address должно быть заполнено.',
-        'string.min': 'Минимальная длина поля address=4.',
-        'string.max': 'Максимальная длина поля address=100.',
-      }),
+    address: Joi.string().required().min(4).max(100).messages({
+      'any.required': 'Поле address должно быть заполнено.',
+      'string.min': 'Минимальная длина поля address=4.',
+      'string.max': 'Максимальная длина поля address=100.',
+    }),
     total: Joi.number().min(1).required().messages({
       'number.min': 'Нельзя оформить бесплатный заказ.',
     }),
